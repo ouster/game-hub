4e2dd5073b75f828332dd5f55ba15d3bc83b2165
Colin Nevin <cnevin@gmail.com>
2024-09-11 13:01:47 +1000

    fixed eslint and prettier, added asses and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import {  Grid, GridItem, Show } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"`
-  }}>
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
-    <Show above="lg">
-    <GridItem area='aside' bg='gold'>Aside</GridItem>
-    </Show>
-    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
-  </Grid> 
-  
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
