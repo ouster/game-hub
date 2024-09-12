6e6c5b3eebb0041495caf60c6b1e606ee4a957b8
Colin Nevin <cnevin@gmail.com>
2024-09-12 16:21:50 +1000

    handle no image games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,6 @@
+import noImage from '../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp';
 const getCroppedImageUrl = (imageUrl: string) => {
-
-  if (!imageUrl) return '';
+  if (!imageUrl) return noImage;
   const target = 'media/';
   const index = imageUrl.indexOf(target) + target.length;
   return imageUrl.slice(0, index) + 'crop/600/400/' + imageUrl.slice(index);
