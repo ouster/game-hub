aa89b1f2b60487377c27f59f032743f5c7a5e323
Colin Nevin <cnevin@gmail.com>
2024-09-12 15:07:54 +1000

    filter by genre

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 import NavBar from './components/NavBar';
 import GameGrid from './components/GameGrid';
 import GenreList from './components/GenreList';
+import { Genre } from './hooks/useGenres';
+import { useState } from 'react';
 
 function App() {
+  const [selectGenre, setSelectGenre] = useState<Genre | null>(null);
+
   return (
     <Grid
       templateAreas={{
@@ -22,11 +26,11 @@ function App() {
 
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList />
+          <GenreList onSelectGenre={(genre) => setSelectGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid />
+        <GameGrid selectedGenre={selectGenre} />
       </GridItem>
     </Grid>
   );
